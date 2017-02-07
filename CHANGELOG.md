cc12070d3d22f381f3a6dc63654005bbf384f6bb
G. Grau <ggrau@indra.es>
2017-02-07 08:41:48 +0100

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1 (Feb 7, 2017)
+
+* Fix directory exclusion on Windows.
+
 ## 1.0.0 (Jan 20, 2017)
 
 * First public release.
--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 // Basic config
 // ===============================================
 const NAME = 'xxl';
-const VERSION = '1.0.0';
+const VERSION = '1.0.1';
 const DESCRIPTION = 'LOC statistics for multiple directories';
 const KEYWORDS = ['loc', 'statistics'];
 
