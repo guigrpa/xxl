9d76b64c65da755aca424413b6d345e7ff15324c
G. Grau <ggrau@me.com>
2017-02-13 15:32:16 +0100

    v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.1.0 (Feb 13, 2017)
+
+* Re-introduce `storyboard` (now with a very fast installation).
+
 ## 1.0.1 (Feb 7, 2017)
 
 * Fix directory exclusion on Windows.
--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 // Basic config
 // ===============================================
 const NAME = 'xxl';
-const VERSION = '1.0.1';
+const VERSION = '1.1.0';
 const DESCRIPTION = 'LOC statistics for multiple directories';
 const KEYWORDS = ['loc', 'statistics'];
 
